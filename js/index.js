2467d0b5cf86b4c1c11bf415a5848903988f809e
charlie  lee <leetochia@gmail.com>
2019-04-10 16:27:20 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,3 +4,11 @@ function getRepositories() {
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+
+function showRepositories() {
+  console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++){
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+}
