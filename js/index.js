deac2be8917926aab2598774e616835562efaff0
charlie  lee <leetochia@gmail.com>
2019-04-10 16:48:55 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -14,6 +14,8 @@ function showRepositories() {
       '<li>' +
       r.name +
       '- <a href="#" data-repo="' +
+      r.name +
+      '" onclick="getCommits(this)">Get Commits</a></li>'
     )
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
